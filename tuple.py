af002dc050e47aebc429547cf35bf13f109761c9
pooja chhikara <chhikarapooja.jetlearn@gmail.com>
2025-05-28 23:39:18 +0530

    Update tuple.py

--- tuple.py
+++ tuple.py
@@ -53,3 +53,61 @@ print(kids3[-5:])
 print(kids3[1:4])
 print(kids3[1:-1])
 print(kids3[-5:-1])
+
+
+# sets 
+
+
+sample_list = [1,1,2,2,3,3]
+
+sample_set = set(sample_list)
+
+print(sample_set)
+
+print(sample_set[2])
+
+
+
+myset = set([])
+myset.add(3)
+myset.add(3)
+myset.add(2)
+myset.add(1)
+
+print(myset)
+
+
+
+myset.remove(2)
+
+myset.remove(5)
+
+myset.discard(5)
+
+print(myset)
+
+#Set Operations
+# 1) Union
+# 2) Intersection
+# 3) Difference
+# 4) Symmetric Difference
+
+
+
+a = {1,2,3,4,5}
+b = {4,5,6,7,8}
+
+
+print(a.union(b))
+print(a | b)
+
+
+print(a.intersection(b))
+print(a & b)
+
+
+print(a.difference(b))
+print(a - b)
+
+print(a.symmetric_difference(b))
+print(a ^ b)
